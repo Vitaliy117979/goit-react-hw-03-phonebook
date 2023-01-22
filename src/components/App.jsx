c498e1fff6823ee74beb2662f6a3fbf1bdbcebec
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-01-22 16:50:53 +0200

    qwerty

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 import { Form } from 'components/Form/Form';
 import { ContactList } from './ContactList/ContactList';
-import { MainWrapper } from './MainWrapper.styled';  
+import { MainWrapper } from './MainWrapper.styled';
 import { Filter } from './Filter/Filter';
 export class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     ],
-    
-    filter: ''
+
+    filter: '',
   };
   onHandlerSubmit = ({ name, number }) => {
     const notify = () => toast(`Sorry, ${name} is already in contacts.`);
     if (this.state.contacts.find(contact => contact.name === name)) {
       notify();
-      
-            return 
-          }
 
+      return;
+    }
 
     this.setState(prevState => ({
       contacts: [...prevState.contacts, { id: nanoid(), name, number }],
@@ -36,42 +35,54 @@ export class App extends Component {
     }));
   };
 
-changeFilter = (e) => {
-this.setState({filter: e.currentTarget.value})
-}
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
 
-
-
-
-getFiltredContacts = () => {
-  const { filter, contacts } = this.state;
-  const normalizedFilter = filter.toLowerCase();
+  getFiltredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
+  };
 
+
+
+  componentDidMount(){
+const getContacts = localStorage.getItem("contacts")
+const parseJsonContacts = JSON.parse(getContacts)
+
+if(parseJsonContacts){this.setState({
+  contacts: parseJsonContacts,
+});}
+
+
+
+  }
+
+componentDidUpdate(_, prevState){
+if(this.state.contacts !== prevState.contacts){
+  localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+}
 
 }
-  render() {
-  const { filter, } = this.state;
 
+  render() {
+    const { filter } = this.state;
 
     console.log(this.state);
     return (
       <MainWrapper>
-        <Toaster   position="top-right"
-  reverseOrder={false}/>
+        <Toaster position="top-right" reverseOrder={false} />
         <h1>Phonebook</h1>
         <Form onSubmit={this.onHandlerSubmit} />
         <h2>Contacts</h2>
-        <Filter value ={filter}
-        onChange = {this.changeFilter}
-        />
-        <ContactList 
-        items={this.getFiltredContacts()}
-        onDeleteItem={this.deleteItem}
-        
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          items={this.getFiltredContacts()}
+          onDeleteItem={this.deleteItem}
         />
       </MainWrapper>
     );
